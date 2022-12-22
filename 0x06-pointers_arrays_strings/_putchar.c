293e137810f194552080404eb4effc78b302a869
Karani <karanimaxwellnjagi@gmail.com>
2022-12-22 02:33:21 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -1,16 +1,13 @@
-#include "main.h"
 #include <unistd.h>
 
 /**
  * _putchar-writes the character c to stdout.
  * @c: The character to print.
- *
- * Return: 1
- * on error, -1 is returned, and error is set appropriately.
+ * Return: 1 on success
  */
 
 int _putchar(char c)
 {
-	return (write(1, Sc, 1));
+	return (write(1, &c, 1));
 
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,13 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * file - main.h
- * auth: Mawell
- * task 0 0x06-pointers_arrays_strings
- */
-
-int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -20,5 +13,6 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+int _putchar(char c);
 
 #endif
