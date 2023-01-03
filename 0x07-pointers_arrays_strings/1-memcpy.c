7a35ef63295ac2eb0153300a83427539d36162e5
Karani <karanimaxwellnjagi@gmail.com>
2023-01-03 03:50:47 -0800

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,7 +7,7 @@
  *
  * Return: copied memory with n byted changed
  */
-char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
 
