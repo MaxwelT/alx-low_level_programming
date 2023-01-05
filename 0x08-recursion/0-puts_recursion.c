318e97d22f5e0780f08d96a54ef76b48fab99d2e
Karani <karanimaxwellnjagi@gmail.com>
2023-01-05 03:11:14 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,11 +2,11 @@
 
 /**
  * _puts_recursion- prints a string followed by new line
- * @5: the string to be printed
+ * @s: the string to be printed
  */
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s != '\0')
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
